32747e4ed70b22bf2837807a43da28640c828311
rocciescobar <roccisen.88@gmail.com>
2018-03-21 20:57:26 -0500

    Añade componentes search y route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,12 @@
 import React from 'react';
+import Search from './Search';
+import Route from './Route'
 
 const Home = () => (
   <div className="mt-7">
-    <form className="">
-      <div className="col-auto">
-        <label className="sr-only">Username</label>
-        <div className="input-group mb-2">
-          <div className="input-group-prepend">
-            <div className="input-group-text"><i className="fas fa-search"></i></div>
-          </div>
-          <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Encuentra lugares" />
-        </div>
-      </div>
-    </form>
-    <div className="col-auto">
-      <p>Te encuentras en: </p>
-      <p>Aquí va el nombre del lugar donde te encuentras.</p>
-    </div>
+    <Search />
     <div className="mapRoute vh-40 bg-dark">aqui el mapa</div>
-    <div className="d-flex justify-content-center my-3">
-      <button type="button" className="btn btn-dark"><i className="fas fa-circle text-danger"></i> Grabar ruta</button>
-    </div>
+    <Route />
   </div>
 )
 export default Home
